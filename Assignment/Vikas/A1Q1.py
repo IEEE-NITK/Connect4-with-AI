b7a3547a0a8b4e2f5e5de8901ad8732baff25f56
Vikas K Bhat <vikaskbhat03@gmail.com>
2021-03-05 22:58:07 +0530

    Second final

--- Assignment/Vikas/A1Q1.py
+++ Assignment/Vikas/A1Q1.py
@@ -1,9 +1,10 @@
 def consec(grid,n,m,w):
     min_div = w-1
-    max_div = n-w+1
+    max_div1 = n-w+1
+    max_div2 = m-w+1
     #horizontal
     for i in range(n):
-        for j in range(max_div):
+        for j in range(max_div2):
             temp = grid[i][j]
             for l in range(w):
                 if(grid[i][j+l] != temp):
@@ -11,8 +12,8 @@ def consec(grid,n,m,w):
                 elif(l == min_div):
                     return temp
     #vertical
-    for i in range(max_div):
-        for j in range(n):
+    for i in range(max_div1):
+        for j in range(m):
             temp = grid[i][j]
             for l in range(w):
                 if(grid[i+l][j] != temp):
@@ -20,8 +21,8 @@ def consec(grid,n,m,w):
                 elif(l == min_div):
                     return temp
     #negative diag
-    for i in range(max_div):
-        for j in range(max_div):
+    for i in range(max_div1):
+        for j in range(max_div2):
             temp = grid[i][j]
             for l in range(w):
                 if(grid[i+l][j+l] != temp):
@@ -30,7 +31,7 @@ def consec(grid,n,m,w):
                     return temp
     #positive diag
     for i in range(min_div,n,1):
-        for j in range(max_div):
+        for j in range(max_div2):
             temp = grid[i][j]
             for l in range(w):
                 if(grid[i-l][j+l] != temp):
