167e301e2fc42e20828b7c26ae493225fbd65d14
siri1404 <72458883+siri1404@users.noreply.github.com>
2021-03-06 19:52:03 +0530

    updated A1Q1.py

--- Assignment/Pooja/A1Q1.py
+++ Assignment/Pooja/A1Q1.py
@@ -1,38 +1,51 @@
-s = input("").split(" ")
-N = int(s[0]); M = int(s[1]); K = int(s[2]); W = int(s[3])
-matrix = []
-for i in range(N):
-    sub_matrix = input("").split(" ")
-    for j in range(len(sub_matrix)):
-        sub_matrix[j] = int(sub_matrix[j])
-    matrix.append(sub_matrix)
-transpose_matrix = []
-for i in range(M):
-    c = []
-    for j in range(N):
-        c.append(0)
-    transpose_matrix.append(c)
-for j in range(len(matrix)):
-    for i in range(len(matrix[0])):
-        transpose_matrix[i][j] = matrix[j][i]
-diagonal_matrix = []
-a = []
-b = []
-for i in range(min(M,N)):
-    b.append(matrix[i][i])
-    a.append(matrix[i][M-i-1])
-diagonal_matrix.append(a)
-diagonal_matrix.append(b)
-combined = matrix + transpose_matrix + diagonal_matrix
-result = []
-for i in combined:
-    for j in (range(len(i)-W + 1)):
-        result.append(list(i[j:j+W]))
-s = 0
-for i in result:
-    if len(set(i)) == 1:
-        print(i[0])
-        s += 1
-        break
-if s == 0:
+l=list(map(int,input().split()))
+n,m,k,w=l[0],l[1],l[2],l[3]
+mat = []
+s=-2
+for i in range(n):
+    b=list(map(int,input().split()))
+    mat.append(b)
+
+# Diagonal
+for i in range(w-1,n,1):
+    for j in range(m-w+1):
+        p = mat[i][j]
+        for l in range(w):
+            if(mat[i-l][j+l] != p):
+                break
+            elif(l == w-1):
+                s = p
+for i in range(n-w+1):
+    for j in range(m-w+1):
+        p = mat[i][j]
+        for l in range(w):
+            if(mat[i+l][j+l] != p):
+                break
+            elif(l == w-1):
+                s=p
+
+#Horizontal
+for i in range(n):
+    for j in range(m-w+1):
+        p = mat[i][j]
+        for l in range(w):
+            if(mat[i][j+l] != p):
+                break
+            elif(l == w-1):
+                s=p
+                   
+#vertical
+for i in range(n-w+1):
+    for j in range(m):
+        p = mat[i][j]
+        for l in range(w):
+            if(mat[i+l][j] != p):
+                break
+            elif(l == w-1):
+                s=p
+if s == -2:
     print(-1)
+else:
+    print(s)
+    
+
